cfbd3e56c159a9f5a3f5c558a7845dcfd1a07546
Mehedi-Hasan-Raju <tecmehedi7@gmail.com>
2026-07-03 00:43:43 +0600

    setting up a fake json database

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import { writeUser, readUser } from "../helpers/fileDb";
 import parseBody from "../helpers/parseBody";
 import addRoute from "../helpers/RouteHandler";
 import sendJson from "../helpers/sendJson";
@@ -18,5 +19,17 @@ addRoute("GET", "/api",(req, res) =>{
 
 addRoute("POST", "/api/users", async (req, res) => {
           const body = await parseBody(req);
+
+           const user = readUser();
+
+          const newUser ={
+            id: Date.now(),
+            ...body
+          };
+
+            user.push(newUser);
+            writeUser(user);
+
+
             sendJson(res, body,201);
 });
